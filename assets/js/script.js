145ad0d120f031f6ada2e851629ac63c9086707e
Russell Morgan <rhmorgan13@comcast.net>
2020-04-06 19:40:41 -0500

    finished github issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 let buttonE1 = document.querySelector("#save-task");
 let tasksToDoE1 = document.querySelector("#tasks-to-do");
 
-buttonE1.addEventListener("click", function() {
+
+let createTaskHandler = function() {
   var listItemE1 = document.createElement("li");
   listItemE1.className = "task-item";
   listItemE1.textContent = "This is a new task.";
   tasksToDoE1.appendChild(listItemE1);
-});
+}
+
+
+buttonE1.addEventListener("click", createTaskHandler);
