d8c5cb7aebb2873a2c1591c4e17d8cd887b361f0
Russell Morgan <rhmorgan13@comcast.net>
2020-04-08 21:19:15 -0500

    refactored the script file, wasn't working as desired

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,24 @@ let tasksToDoE1 = document.querySelector("#tasks-to-do");
 let createTaskHandler = function(event) {
 
   event.preventDefault();
+  let taskNameInput = document.querySelector("input[name='task-name']").value;
+  let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  let listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+  // create list item
+  var listItemE1 = document.createElement("li");
+  listItemE1.className = "task-item";
+
+  // create div to hold task info and add to list item
+  var taskInfoE1 = document.createElement("div");
+  // give it a class name
+  taskInfoE1.className = "task-info";
+  // add HTML content to div
+  taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemE1.appendChild(taskInfoE1);
+
+  // add entire list item to list
+  tasksToDoE1.appendChild(listItemE1);
 };
 
 formE1.addEventListener("submit", createTaskHandler);
